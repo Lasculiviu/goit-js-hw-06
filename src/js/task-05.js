128c038932c091a21b9e3d3693b22d8bb7249b2a
Lasculiviu <lasculiviu131@gmail.com>
2024-06-15 11:12:36 +0200

    5

---
+++ src/js/task-05.js
@@ -0,0 +1,12 @@
+
+const nameInput = document.getElementById('name-input');
+const nameOutput = document.getElementById('name-output');
+
+
+const updateNameOutput = () => {
+  const inputValue = nameInput.value.trim(); 
+  nameOutput.textContent = inputValue === '' ? 'Anonymous' : inputValue;
+};
+
+
+nameInput.addEventListener('input', updateNameOutput);
