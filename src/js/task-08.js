b06831648dc7e504fb7e69f0d2c8f638123ed87f
Lasculiviu <lasculiviu131@gmail.com>
2024-06-15 11:48:45 +0200

    8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector('.login-form');
+loginForm.addEventListener('submit', function(event) {
+  event.preventDefault(); 
+  const email = loginForm.elements['email'].value;
+  const password = loginForm.elements['password'].value;
+  if (email === '' || password === '') {
+    alert('Toate câmpurile trebuie completate!');
+    return;
+  }
+  const formData = {
+    email: email,
+    password: password
+  };
+  console.log(formData);
+  loginForm.reset();
+});
 
